1f4839501212034b81c334d5fa52340764402ee0
leo-pony <nengjunma@outlook.com>
2025-07-09 11:55:20 +0800

    Remove unused patch_utils
    Signed-off-by: leo-pony <nengjunma@outlook.com>

--- vllm_ascend/models/qwen3_moe_bak.py
+++ vllm_ascend/models/qwen3_moe_bak.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
-# Copyright 2023 The vLLM team.
-#
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,23 +10,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Adapted from vllm/model_executor/models/qwen3_moe.py
 # This file is a part of the vllm-ascend project.
+#
 
-from vllm.model_executor.models.qwen3_moe import Qwen3MoeForCausalLM
+import unittest
+
+from vllm_ascend.utils import adapt_patch
 
 
-class CustomQwen3MoeForCausalLM(Qwen3MoeForCausalLM):
-    packed_modules_mapping = {
-        "qkv_proj": [
-            "q_proj",
-            "k_proj",
-            "v_proj",
-        ],
-        "gate_up_proj": [
-            "gate_proj",
-            "up_proj",
-        ],
-        "experts":
-        ["experts.0.gate_proj", "experts.0.up_proj", "experts.0.down_proj"],
-    }
+class TestBase(unittest.TestCase):
+
+    def setUp(self):
+        # adapt patch by default.
+        adapt_patch(True)
+        adapt_patch()
+        super().setUp()
